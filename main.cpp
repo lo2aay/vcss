76a3877eb26d30f6aa88ae00b521012711f07422
lo2aay <100815503+lo2aay@users.noreply.github.com>
2023-11-03 19:04:51 +0200

    last

--- main.cpp
+++ main.cpp
@@ -9,12 +9,22 @@ int getSum(const std::vector<int>& numbers) {
     return sum;
 }
 
+double getAverage(const std::vector<int>& numbers) {
+    if (numbers.empty()) {
+        return 0.0; // To avoid division by zero, return 0 for an empty vector.
+    }
+    int sum = getSum(numbers);
+    return static_cast<double>(sum) / numbers.size();
+}
+
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     int sum = getSum(numbers);
+    double average = getAverage(numbers);
 
     std::cout << "Sum of elements in the vector: " << sum << std::endl;
+    std::cout << "Average of elements in the vector: " << average << std::endl;
 
     return 0;
 }
